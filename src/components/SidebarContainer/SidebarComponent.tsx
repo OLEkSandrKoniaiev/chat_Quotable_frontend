ab6d75a4fed517e5ea04fcbf3ac5c2b79c780b98
OLEkSandrKoniaiev <oleksandrkoniaiev.r@gmail.com>
2025-11-03 09:21:53 +0200

    feat: implement basic toast notification

--- src/components/SidebarContainer/SidebarComponent.tsx
+++ src/components/SidebarContainer/SidebarComponent.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 import SidebarHeaderComponent from '../SidebarHeaderContainer/SidebarHeaderComponent.tsx';
 import ChatList from '../ChatListContainer/ChatListComponent.tsx';
@@ -6,13 +7,51 @@ import { useDebounce } from '../../hooks/useDebounce.ts';
 import { NewChatModal } from '../Modals/NewChatModal.tsx';
 import { useGetChatsQuery } from '../../store/services/chatApi.ts';
 import styles from './SidebarComponent.module.css';
+import type { IChat } from '../../interfaces/chat.interfaces.ts';
 
 function SidebarComponent() {
   const [searchQuery, setSearchQuery] = useState('');
   const debouncedSearchQuery = useDebounce(searchQuery, 500);
   const [isNewChatModalOpen, setIsNewChatModalOpen] = useState(false);
 
-  const { data: chats = [], isLoading, error } = useGetChatsQuery(debouncedSearchQuery);
+  const {
+    data: chats = [],
+    isLoading,
+    error,
+  } = useGetChatsQuery(debouncedSearchQuery, {
+    pollingInterval: 5000,
+  });
+
+  const prevChatsRef = useRef<IChat[]>([]);
+
+  useEffect(() => {
+    if (isLoading || prevChatsRef.current.length === 0) {
+      prevChatsRef.current = chats;
+      return;
+    }
+
+    for (const newChat of chats) {
+      const oldChat = prevChatsRef.current.find((c) => c._id === newChat._id);
+
+      if (oldChat && newChat.lastMessageTimestamp !== oldChat.lastMessageTimestamp) {
+        if (newChat.unreadCount > 0) {
+          toast.info(
+            <div>
+              <strong>
+                {newChat.firstName} {newChat.lastName}
+              </strong>
+              <br />
+              {newChat.lastMessage}
+            </div>,
+          );
+        }
+
+        break;
+      }
+    }
+
+    prevChatsRef.current = chats;
+  }, [chats, isLoading]);
 
   const handleSearchChange = (query: string) => {
     setSearchQuery(query);
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { store } from './store/store.ts';
 import { router } from './router/router.tsx';
 import './index.css';
@@ -11,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
+      <ToastContainer position="bottom-right" autoClose={4000} theme="colored" />
     </Provider>
   </StrictMode>,
 );
